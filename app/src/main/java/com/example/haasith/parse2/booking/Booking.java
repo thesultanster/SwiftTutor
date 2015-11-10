094835389f1c7eed70edc25c9fabe2e50212302f
Sultan Khan <khansultan024@gmail.com>
2015-11-09 21:29:53 -0800

    Pressing "OK" on tipCard hides card in Booking activity

--- app/src/main/java/com/example/haasith/parse2/booking/Booking.java
+++ app/src/main/java/com/example/haasith/parse2/booking/Booking.java
@@ -3,9 +3,14 @@ package com.example.haasith.parse2.booking;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.haasith.parse2.R;
 import com.google.android.gms.maps.GoogleMap;
+
+import org.w3c.dom.Text;
 
 
 /*
@@ -17,21 +22,48 @@ import com.google.android.gms.maps.GoogleMap;
 
 public class Booking extends AppCompatActivity {
 
+    // Map Variables
     private NestedScrollView mScrollView;
     private GoogleMap map;
+
+    // Tip Card Variables
+    private LinearLayout tipCard;
+    private TextView okText;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
 
+        InflateVariables();
+        SetClickListeners();
+
+    }
+
+    private void InflateVariables() {
+
+        // Map Variables
         map = ((BookingMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
         mScrollView = (NestedScrollView) findViewById(R.id.scrollView);
+        ((BookingMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).setListener(new BookingMapFragment.OnTouchListener() { @Override public void onTouch() { mScrollView.requestDisallowInterceptTouchEvent(true); }  });
 
-        ((BookingMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).setListener(new BookingMapFragment.OnTouchListener() {
+        // Tip Card Variables
+        okText = (TextView) findViewById(R.id.okText);
+        tipCard = (LinearLayout) findViewById(R.id.tipCard);
+
+
+
+
+    }
+
+    private void SetClickListeners(){
+
+        okText.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onTouch() {
-                mScrollView.requestDisallowInterceptTouchEvent(true);
+            public void onClick(View v) {
+                tipCard.setVisibility(View.GONE);
             }
         });
 
--- app/src/main/java/com/example/haasith/parse2/booking/BookingMapFragment.java
+++ app/src/main/java/com/example/haasith/parse2/booking/BookingMapFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.SupportMapFragment;
 
